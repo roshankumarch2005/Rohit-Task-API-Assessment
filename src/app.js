8ccc75aedc09b783db3a6273a628d7e355c6b6fc
roshankumar chellagonda <roshankumarchellagonda05@gmail.com>
2026-04-16 01:05:26 +0530

    feat: add root welcome route

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
-const express = require('express');
-const taskRoutes = require('./routes/tasks');
+const express = require("express");
+const taskRoutes = require("./routes/tasks");
 
 const app = express();
 
 app.use(express.json());
-app.use('/tasks', taskRoutes);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to the Task API! Please use the /tasks endpoint.",
+  });
+});
+
+app.use("/tasks", taskRoutes);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ error: 'Internal server error' });
+  res.status(500).json({ error: "Internal server error" });
 });
 
 const PORT = process.env.PORT || 3000;
